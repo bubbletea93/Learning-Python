f95393eecd48a6687c853d2e2b29bf00a1dc7670
bubbletea93 <dcnguyen93@yahoo.com>
2021-03-03 11:08:39 -0500

    Add cube function

--- python-fundamentals/functions.py
+++ python-fundamentals/functions.py
@@ -2,5 +2,11 @@
 def sayHi(name):
     print("Hi, " + name + ".")
 
+def calculateCube(number):
+    return number * number * number
+
 name = input("Enter name: ")
 sayHi(name)
+number = float(input("Enter number to cube: "))
+numberCubed = calculateCube(number)
+print(str(number) + " cubed is " + str(numberCubed))
