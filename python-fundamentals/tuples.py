f210c6188738d951bffc4dc972fa6de2535b44fb
bubbletea93 <dcnguyen93@yahoo.com>
2021-03-03 11:02:44 -0500

    Learning tuples

---
+++ python-fundamentals/tuples.py
@@ -0,0 +1,12 @@
+# Type of data structure
+# It's like a container where we can store different values
+# Tuples are immutable meaning it can't be changed or modified.
+coordinates = (4, 5)
+
+# Accessing tuple values
+print(coordinates[0])
+print(coordinates[1])
+
+# Tuple list
+coordinatesList = [(1, 2), (3, 4), (5, 6)]
+print(coordinatesList)
