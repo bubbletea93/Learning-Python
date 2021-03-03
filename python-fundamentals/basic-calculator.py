302c9429485c393a72c0b1750472c3d2076f8dca
bubbletea93 <dcnguyen93@yahoo.com>
2021-03-02 21:53:23 -0500

    Add basic calculator

---
+++ python-fundamentals/basic-calculator.py
@@ -0,0 +1,6 @@
+# number1 and number2 will be inputs as string values
+# Convert number1 and number2 to numbers
+number1 = input("Enter number for variable 1: ")
+number2 = input("Enter number for variable 2: ")
+sum = float(number1) + float(number2)
+print(str(number1) + " + " + str(number2) + " = " + str(sum))
